a1a0dee8f103933e546ac8a77f6d7a2b0e540b7a
Matt Brennan <mattyb1000@gmail.com>
2016-08-04 21:27:10 +0100

    supress find error output

--- index.mk
+++ index.mk
@@ -3,7 +3,7 @@ $(eval $(register-npm-bin))
 babel-src-dir = src
 babel-dst-dir = lib
 
-~babel-src-files = $(shell find $(babel-src-dir) -name '*.js' -o -name '*.jsx')
+~babel-src-files = $(shell find $(babel-src-dir) -name '*.js' -o -name '*.jsx' 2>/dev/null)
 ~babel-dst-files = $(patsubst $(babel-src-dir)/%, $(babel-dst-dir)/%, $(~babel-src-files))
 
 babel-opts =
